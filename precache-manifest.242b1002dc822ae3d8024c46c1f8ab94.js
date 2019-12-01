fdb1f80b80fdf8ebe64f32068fa295e3d7d61491
George <collectionpiter@gmail.com>
2019-12-01 11:17:04 +0300

    Updates

---
+++ precache-manifest.242b1002dc822ae3d8024c46c1f8ab94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b22f9a4c48fbb5a77f959cd8da728a",
+    "url": "/PostGetter/index.html"
+  },
+  {
+    "revision": "1c81cf4cb1153c4bb468",
+    "url": "/PostGetter/static/css/main.cc2d884b.chunk.css"
+  },
+  {
+    "revision": "b985dab37428c3925a80",
+    "url": "/PostGetter/static/js/2.1e5a2dd6.chunk.js"
+  },
+  {
+    "revision": "1c81cf4cb1153c4bb468",
+    "url": "/PostGetter/static/js/main.240b9148.chunk.js"
+  },
+  {
+    "revision": "8d0ce011d1a3049e79a8",
+    "url": "/PostGetter/static/js/runtime-main.ce369d80.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/PostGetter/static/media/logo.25bf045c.svg"
+  }
+]);
